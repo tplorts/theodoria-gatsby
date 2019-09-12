7bf5cf05ae47482b54cc2e1b9bf7fdcdd9085d7f
Ted Lorts <tplorts@gmail.com>
2019-09-12 12:00:54 -0700

    set site & project metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Ted Lorts, Software Developer`,
+    description: `Senior software developer with several years’ experience utilizing a broad technical expertise`,
+    author: `@tplorts`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
