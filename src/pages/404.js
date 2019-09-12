d4f1cc988fbf60ad97a584060a6666bdd6c6ad5e
Ted Lorts <tplorts@gmail.com>
2019-09-12 12:02:40 -0700

    link back to root from 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,5 @@
+import { Link } from "gatsby"
 import React from "react"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -7,7 +7,8 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>You just hit a route that does not exist.</p>
+    <Link to="/">Back to homepage</Link>
   </Layout>
 )
 
